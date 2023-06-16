ae13d4e0334aabea8f49347f5ee883d111af7e63
Arno Manukian <arnas.manucan@gmail.com>
2023-06-16 10:35:22 +0300

    Build Service layer for Employee

---
+++ src/main/java/com/example/hiringmanager/employee/EmployeeService.java
@@ -0,0 +1,94 @@
+package com.example.hiringmanager.employee;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+	private final SessionFactory sessionFactory;
+	
+	@Autowired
+	public EmployeeService(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	// GET employees
+	public final List<Employee> getEmployees() {
+		try (Session session = sessionFactory.openSession()) {
+			return session.createQuery("FROM employee", Employee.class).list();
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to get employees", e);
+		}
+	}
+	
+	// ADD employee
+	public void addNewEmployee(Employee employee) {
+		try (Session session = sessionFactory.openSession()) {
+			session.beginTransaction();
+			Employee existingEmployee = session.get(Employee.class, employee.getEmployeeId());
+			if (existingEmployee != null) {
+				throw new RuntimeException("Employee with ID: " + employee.getEmployeeId() + " already exists");
+			}
+			session.save(employee);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to add a new employee", e);
+		}
+	}
+	
+	// DELETE employee
+	public void deleteEmployee(int employeeId) {
+		try (Session session = sessionFactory.openSession()) {
+			session.beginTransaction();
+			Employee existingEmployee = session.get(Employee.class, employeeId);
+			if (existingEmployee == null) {
+				throw new RuntimeException("Employee with ID: " + employeeId + " doesn't exist");
+			}
+			session.delete(existingEmployee);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to delete the employee", e);
+		}
+	}
+	
+	// UPDATE employee
+	public void updateEmployee(Employee employee) {
+		try (Session session = sessionFactory.openSession()) {
+			session.beginTransaction();
+			Employee existingEmployee = session.get(Employee.class, employee.getEmployeeId());
+			if (existingEmployee == null) {
+				throw new RuntimeException("Employee with ID: " + employee.getEmployeeId() + " doesn't exists");
+			}	
+			session.update(existingEmployee);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to update the employee", e);
+		}
+	}
+	
+	// CALCULATE average salary by department ID
+	public double calculateAvgSalary(int departmentId) {
+	    try (Session session = sessionFactory.openSession()) {
+	    	// Get count of employees with given department id
+	        Query<Long> countQuery = session.createQuery("SELECT COUNT(e) FROM Employee e WHERE e.departmentId = :departmentId", Long.class);
+	        countQuery.setParameter("departmentId", departmentId);
+	        Long employeeCount = countQuery.getSingleResult();
+	        // check if given department has any employees
+	        if (employeeCount == 0) {
+	            throw new RuntimeException("No employees found for department ID: " + departmentId);
+	        }
+	        
+	        Query<Double> query = session.createQuery("SELECT AVG(e.salary) FROM Employee e WHERE e.departmentId = :departmentId", Double.class);
+	        query.setParameter("departmentId", departmentId);
+	        return query.getSingleResult();
+	    } catch (Exception e) {
+	        throw new RuntimeException("Failed to calculate the average salary for department ID: " + departmentId, e);
+	    }
+	}
+
+}

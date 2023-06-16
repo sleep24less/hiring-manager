2d9916f62d1097680e84a64ad4092a525230d6f0
Arno Manukian <arnas.manucan@gmail.com>
2023-06-16 08:50:07 +0300

    Initial commit

---
+++ src/main/java/com/example/hiringmanager/HiringManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hiringmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiringManagerApplication.class, args);
+	}
+
+}
